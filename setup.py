f4b661e013963bbe5e2a8ee356c7e70d3772b794
bmpenuelas <bmpenuelas@gmail.com>
2017-12-07 00:05:30 +0100

    Trying .rst for documentation.

--- setup.py
+++ setup.py
@@ -3,9 +3,13 @@ from setuptools import setup
 setup(name='orbbit',
       version='0.1',
       description='A language-independent API for cryptocurrency trading robots.',
+      long_description=readme(),
       url='https://github.com/bmpenuelas/OrbBit',
       author='The best people.',
       author_email='bmpenuelas@gmail.com',
       license='Closed source',
       packages=['orbbit'],
+      install_requires=[
+          'datetime',
+      ],
       zip_safe=False)
