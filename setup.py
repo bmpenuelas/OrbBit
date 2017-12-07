03e6e724ab7b0475cc54ed78ec3c7ff15e36d714
Borja Penuelas <bmpenuelas@gmail.com>
2017-12-07 15:24:26 +0100

    Created example file structure (#1)
    * Test package structure.
    
    * Trying .rst for documentation.
    
    * Add example tags.
    
    * Adding __main__ structure.
    
    * Fix example __init__

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='orbbit',
+      version='0.1',
+      description='A language-independent API for cryptocurrency trading robots.',
+      long_description=readme(),
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+      ],
+      keywords='cryptocurrency trading robots',
+      url='https://github.com/bmpenuelas/OrbBit',
+      author='The best people.',
+      author_email='bmpenuelas@gmail.com',
+      license='Closed source',
+      packages=['orbbit'],
+      include_package_data=True,
+      install_requires=[
+          'datetime',
+      ],
+      scripts=['bin/start_hi'],
+      # entry_points={
+      #     'console_scripts': [
+      #         'start_main = my_project.__main__:main'
+      #     ]
+      # },
+      zip_safe=False)
